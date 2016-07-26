f34c1afffc5d631757e4bd8f1c39d3e1993763e0
msksfo <TiaEsguerra@tias-mbp.attlocal.net>
2016-07-26 08:45:23 -0700

    cast to float

--- CodeAppetizer_GitHub/billCalculator.py
+++ CodeAppetizer_GitHub/billCalculator.py
@@ -5,8 +5,8 @@
 
 bill = raw_input("How much was your bill?")
 
-tip = bill * .18
+tip = float (bill) * .18
 
-total_bill = bill + tip
+total_bill = float (bill) + tip
 
 print "The tip is %f and the total bill is %f ." % (tip, total_bill)
